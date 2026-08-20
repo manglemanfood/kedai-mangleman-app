eddec156aa496876518fe4737b62f08f3ca19cac
manglemanfood <mangleman.food@gmail.com>
2026-08-20 17:13:32 +0700

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://beogjzmgpadtdzchpuap.supabase.co'
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzZnZybGZza2RicmR3amh2ZGVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5Mzc4MjAsImV4cCI6MjA5NzUxMzgyMH0.9MV6KtW9ZGwLbvr1pJnj9g5SkrmGhxQU0igyrP-d0LM'
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlb2dqem1ncGFkdGR6Y2hwdWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4ODM2OTQsImV4cCI6MjA5NzQ1OTY5NH0.g93PZvj2asN6_jSE4djE2WUmMEX-BPqbIaD2flwLML4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
